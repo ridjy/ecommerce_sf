f5843ad66465378b02978cc5fef08f67324d0279
ridjy <r.ridjy@gmail.com>
2022-07-28 20:26:24 +0300

    utilisation du password encoder

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\CallbackTransformer;
 
 class UserType extends AbstractType
 {
@@ -16,6 +17,15 @@ class UserType extends AbstractType
             ->add('roles')
             ->add('password')
         ;
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesAsArray) {
+                    return count($rolesAsArray) ? $rolesAsArray[0]: null;
+                },
+                function ($rolesAsString) {
+                    return [$rolesAsString];
+                }
+        ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
