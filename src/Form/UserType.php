9f0d3aa50778933e2af78cfeb6a29fa87972f384
ridjy <r.ridjy@gmail.com>
2022-07-28 22:13:01 +0300

    inscription ok

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,15 +9,27 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\CallbackTransformer;
 //ajout du use pour utiliser le type input password de Symfony
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username',TextType::class,array(
+                'label' => 'Vorname',
+                'attr' => array(
+                    'placeholder' => 'Votre pseudo')))
+            ->add('email',EmailType::class,array(
+                'label' => 'Vormail',
+                'attr' => array(
+                    'placeholder' => 'Votre adresse mail')))
             //->add('roles') //à ajouter par defaut
-            ->add('password', PasswordType::class)
+            ->add('password', PasswordType::class,array(
+                'label' => 'Vormdp',
+                'attr' => array(
+                    'placeholder' => 'Votre mot de passe')))
         ;
         /*$builder->get('roles')
             ->addModelTransformer(new CallbackTransformer(
