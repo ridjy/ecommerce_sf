6a3a99444dea4da40f29742215c4884664fcbb76
Eddy <eddy@qdata-mg.com>
2022-08-01 17:57:10 +0300

    lien easy admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User; 
+use App\Entity\Categorie;
+use App\Entity\CodePromo;
+use App\Entity\Commande;
+use App\Entity\Produit;    
 
 class DashboardController extends AbstractDashboardController
 {
@@ -28,5 +33,12 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Blog');
+        yield MenuItem::linkToCrud('User', 'fa fa-tags', User::class);
+        yield MenuItem::linkToCrud('Categorie', 'fa fa-file-text', Categorie::class);
+
+        yield MenuItem::section('Users');
+        yield MenuItem::linkToCrud('CodePromo', 'fa fa-comment', CodePromo::class);
+        yield MenuItem::linkToCrud('Produits', 'fa fa-user', Produit::class);
     }
 }
