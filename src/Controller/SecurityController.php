3805a22fddf4e5201b01768cadbe229b10c87424
Eddy <eddy@qdata-mg.com>
2022-08-11 12:08:26 +0300

    authentification google scope à regler

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -46,6 +46,52 @@ class SecurityController extends AbstractController
     }
 
     /**
+     * Link to this controller to start the "connect" process
+     *
+     * @Route("/connect/google", name="app_loginGoogle")
+     */
+    public function connectGoogle(ClientRegistry $clientRegistry)
+    {
+        return $clientRegistry
+            ->getClient('google') // key used in config/packages/knpu_oauth2_client.yaml
+            ->redirect([
+                'public_profile', 'email' // the scopes you want to access
+            ]);
+    }
+
+    /**
+     * After going to Facebook, you're redirected back here
+     * because this is the "redirect_route" you configured
+     * in config/packages/knpu_oauth2_client.yaml
+     *
+     * @Route("/login/check-google", name="connect_google_check")
+     */
+    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
+    {
+        // ** if you want to *authenticate* the user, then
+        // leave this method blank and create a Guard authenticator
+        // (read below)
+
+        /** @var \KnpU\OAuth2ClientBundle\Client\Provider\Google $client */
+        $client = $clientRegistry->getClient('google');
+
+        try {
+            // the exact class depends on which provider you're using
+            /** @var \League\OAuth2\Client\Provider\FacebookUser $user */
+            $user = $client->fetchUser();
+
+            // do something with all this new power!
+            // e.g. $name = $user->getFirstName();
+            var_dump($user); die;
+            // ...
+        } catch (IdentityProviderException $e) {
+            // something went wrong!
+            // probably you should return the reason to the user
+            var_dump($e->getMessage()); die;
+        }
+    }
+
+    /**
      * @Route("/logout", name="app_logout")
      */
     public function logout(): void
