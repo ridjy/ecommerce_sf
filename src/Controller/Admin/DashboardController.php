bc91030b9be60b1684d54dfb1938a16d1bd0d37f
Eddy <eddy@qdata-mg.com>
2022-08-17 15:50:15 +0300

    ajout fct dans le dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -11,11 +11,14 @@ use App\Entity\User;
 use App\Entity\Categorie;
 use App\Entity\CodePromo;
 use App\Entity\Commande;
-use App\Entity\Produit;    
+use App\Entity\Produit;  
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use Symfony\Component\Security\Core\User\UserInterface;  
 
 class DashboardController extends AbstractDashboardController
 {
     /**
+     * 
      * @Route("/admin", name="admin")
      */
     public function index(): Response
@@ -32,13 +35,37 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToRoute('Site web', 'fa fa-home', 'app_home');
+        yield MenuItem::linkToRoute('Site web', 'fa fa-shopping-bag', 'app_home');
         yield MenuItem::section('Blog');
         yield MenuItem::linkToCrud('User', 'fa fa-user', User::class);
+        yield MenuItem::section('Boutique');
         yield MenuItem::linkToCrud('Categorie', 'fa fa-file-text', Categorie::class);
+        yield MenuItem::linkToCrud('Produits', 'fa fa-database', Produit::class);
+        yield MenuItem::linkToCrud('CodePromo', 'fa fa-code', CodePromo::class);
+    }
 
-        yield MenuItem::section('Users');
-        yield MenuItem::linkToCrud('CodePromo', 'fa fa-tags', CodePromo::class);
-        yield MenuItem::linkToCrud('Produits', 'fa fa-user', Produit::class);
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        // Usually it's better to call the parent method because that gives you a
+        // user menu with some menu items already created ("sign out", "exit impersonation", etc.)
+        // if you prefer to create the user menu from scratch, use: return UserMenu::new()->...
+        return parent::configureUserMenu($user)
+            // use the given $user object to get the user name
+            ->setName($user->getUserName())
+            // use this method if you don't want to display the name of the user
+            ->displayUserName(true)
+
+            // you can return an URL with the avatar image
+            ->setAvatarUrl('https://...')
+            // use this method if you don't want to display the user image
+            ->displayUserAvatar(false)
+            // you can also pass an email address to use gravatar's service
+            ->setGravatarEmail($user->getEmail())
+
+            // you can use any type of menu item, except submenus
+            ->addMenuItems([
+                MenuItem::linkToRoute('My Profile', 'fa fa-id-card', '...', ['...' => '...']),
+                MenuItem::linkToRoute('Settings', 'fa fa-user-cog', '...', ['...' => '...']),
+            ]);
     }
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,6 +13,31 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            // the names of the Doctrine entity properties where the search is made on
+            // (by default it looks for in all properties)
+            ->setSearchFields(['username', 'email'])
+
+            // defines the initial sorting applied to the list of entities
+            // (user can later change this sorting by clicking on the table columns)
+            ->setDefaultSort(['id' => 'DESC'])
+
+            // the max number of entities to display per page
+            ->setPaginatorPageSize(15)
+            // the number of pages to display on each side of the current page
+            // e.g. if num pages = 35, current page = 7 and you set ->setPaginatorRangeSize(4)
+            // the paginator displays: [Previous]  1 ... 3  4  5  6  [7]  8  9  10  11 ... 35  [Next]
+            // set this number to 0 to display a simple "< Previous | Next >" pager
+            ->setPaginatorRangeSize(4)
+
+            // these are advanced options related to Doctrine Pagination
+            // (see https://www.doctrine-project.org/projects/doctrine-orm/en/2.7/tutorials/pagination.html)
+            ->setPaginatorUseOutputWalkers(true)
+            ->setPaginatorFetchJoinCollection(true)
+        ;
+    }
     /*
     public function configureFields(string $pageName): iterable
     {
