76e220b02f880a957f300fcea41a6d407533526b
Eddy <eddy@qdata-mg.com>
2022-08-17 17:57:10 +0300

    ajout upload file

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,6 +11,8 @@ use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class UserType extends AbstractType
 {
@@ -30,6 +32,25 @@ class UserType extends AbstractType
                 'label' => 'Vormdp',
                 'attr' => array(
                     'placeholder' => 'Votre mot de passe')))
+            ->add('avatar', FileType::class, [
+                'label' => 'Votre avatar (PNG ou Jpeg)',
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+                // make it optional so you don't have to re-upload the file
+                'required' => false,
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Veuiller uploader une image valable',
+                    ])
+                ],
+            ])
         ;
         /*$builder->get('roles')
             ->addModelTransformer(new CallbackTransformer(
